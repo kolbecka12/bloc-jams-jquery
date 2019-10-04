0c5de37bb3797ef2ff82fec82d5f909f536ef0a2
kolbecka12 <kolbecka@gmail.com>
2019-10-04 14:42:16 -0700

    first commit

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,1 +1,34 @@
+{
+  album.songs.forEach( (song, index) => {
+    song.element = $(`
+      <tr>
+        <td>index</td>
+        <td>song.title</td>
+        <td>song.duration</td>
+        <td>
+          <button>
+            <span class="song-number">${index + 1 }</span>
+            <span class="ion-play"></span>
+            <span class="ion-pause"></span>
+          </button>
+        <td>${song.title}</td>
+        <td>${song.duration}</td>
+       </tr>
+    `);
+    song.element.on('click', event => {
+      player.playPause(song);
+    });
+
+    $('#song-list').append(song.element);
+  });
+}
+
+
+
+
+
+
+
+
+
 // this script is blank. We'll use it to fulfill the second user story by listing the songs on the album and playing/pausing songs on click.
